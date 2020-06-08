39e93951f950e29d4b47acae0443a49f83a67347
Vishwas Basotra <brigu.sharma.9@gmail.com>
2020-06-09 00:30:02 +0530

    opening image with openCV

--- 01-Image-Basics-with-OpenCV/01-Opening-Image-Files-OpenCV.py
+++ 01-Image-Basics-with-OpenCV/01-Opening-Image-Files-OpenCV.py
@@ -1,16 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 00:07:39 2020
+
+@author: Vishwas Basotra
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
-img = cv2.imread('DATA/00-puppy.jpg')
+img = cv2.imread('../DATA/00-puppy.jpg')
 
 while True:
-
     cv2.imshow('Puppy',img)
-
-    # EXPLANATION FOR THIS LINE OF CODE:
-    # https://stackoverflow.com/questions/35372700/whats-0xff-for-in-cv2-waitkey1/39201163
-    
-    # IF we've waited at least 1 ms AND we've pressed the Esc
+    # if we waited atleast 1 ms AND we've pressed Esc key
     if cv2.waitKey(1) & 0xFF == 27:
         break
-
 cv2.destroyAllWindows()
